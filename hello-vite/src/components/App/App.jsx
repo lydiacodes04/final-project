5e96ebd1b66b3e8f55da011d704db6c0b7878c1d
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-06-10 21:30:13 -0500

    add normalize.css and .prettierignore

--- hello-vite/src/components/App/App.jsx
+++ hello-vite/src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "../../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
@@ -17,18 +17,15 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/saved-resources" element={<SavedResources />} />
         <Route path="*" element={<Main />} />
-      </Routes>
+      </Routes> */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
