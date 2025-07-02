1a1076be76f3fdd6c41a063c2d49eb872d81fbdb
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-07-01 21:51:34 -0500

    update ItemCard CSS

--- hello-vite/src/components/ItemCard/ItemCard.jsx
+++ hello-vite/src/components/ItemCard/ItemCard.jsx
@@ -4,14 +4,17 @@ function ItemCard({ singleResource }) {
   return (
     <li className="resourceCard">
       <h2 className="resourceCard__name">{singleResource.Name}</h2>
-      <h2 className="resourceCard_type">{singleResource.ProgramType}</h2>
-      <h2 className="resourceCard__address">
-        {singleResource.Address1},{singleResource.City},
-        {singleResource.StateAbbrev},{singleResource.Zip}
-      </h2>
-      <h2 className="resourceCard_distance">{singleResource.Distance}</h2>
+      <h3 className="resourceCard__type"> {singleResource.ProgramType}</h3>
+      <h4 className="resourceCard__address">
+        {singleResource.Address1},{singleResource.City},{singleResource.Zip}
+      </h4>
+      <h5 className="resourceCard__distance">{singleResource.Distance}</h5>
     </li>
   );
 }
 
 export default ItemCard;
+
+// removed, as it wasn't showing:
+// {singleResource.StateAbbrev}
+// {singleResource.Address2}
