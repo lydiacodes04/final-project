fa1907642521bca547ade013779651e74e80d152
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-07-02 18:30:54 -0500

    format Header

--- hello-vite/src/components/Header/Header.jsx
+++ hello-vite/src/components/Header/Header.jsx
@@ -13,8 +13,8 @@ function Header() {
         </Link>
 
         <Link to="/profile" className="header__about">
-          <p className="header__about"> About </p>
-          <img className="header__avatar" src={chicken} alt="avatar" />
+          <p className="header__about-text"> About </p>
+          <img className="header__about-avatar" src={chicken} alt="avatar" />
         </Link>
       </div>
     </header>
