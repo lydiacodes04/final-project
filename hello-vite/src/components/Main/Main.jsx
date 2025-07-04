555d7981be908779bb54a061b5f793e582df10d3
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-07-03 19:36:08 -0500

    finish moving SearchForm out of Main into its own component docs

--- hello-vite/src/components/Main/Main.jsx
+++ hello-vite/src/components/Main/Main.jsx
@@ -6,31 +6,19 @@ function Main({ resource, onZipSubmit, handleInputChange, inputValue }) {
   return (
     <main>
       <section className="search">
-        <form action="" className="search__bar" onSubmit={onZipSubmit}>
-          <input
-            className="search__fieldset"
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder="search for resources"
-          ></input>
-          <button className="search__submit-button" type="submit">
-            SEARCH
-          </button>
-        </form>
-
         <p className="search__text">
           Search for resources near you. The cards below show resources in the
           zipcode you searched. There will be a maximum of 6 to start out with
           and then more if you click "show more." For example, try zipcodes
           99504 (Anchorage, AK), 78207 (San Antonio, TX), or 95403 (Santa Rosa,
           California).
-          {/* Search for resources near you. Imagine cards appearing here. there
-          would be maybe 6 to start out with and then more if you click "show
-          more" */}
         </p>
       </section>
-
+      <SearchForm
+        onZipSubmit={onZipSubmit}
+        handleInputChange={handleInputChange}
+        inputValue={inputValue}
+      />
       {resource.map((singleResource) => {
         return (
           <ItemCard singleResource={singleResource} key={singleResource.Name} />
--- hello-vite/src/components/SearchForm/SearchForm.jsx
+++ hello-vite/src/components/SearchForm/SearchForm.jsx
@@ -2,20 +2,18 @@ import "./SearchForm.css";
 
 function SearchForm({ onZipSubmit, handleInputChange, inputValue }) {
   return (
-    <section className="search">
-      <form action="" className="search__bar" onSubmit={onZipSubmit}>
-        <input
-          className="search__fieldset"
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          placeholder="search for resources"
-        ></input>
-        <button className="search__submit-button" type="submit">
-          SEARCH
-        </button>
-      </form>
-    </section>
+    <form action="" className="search__bar" onSubmit={onZipSubmit}>
+      <input
+        className="search__fieldset"
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="search for resources"
+      ></input>
+      <button className="search__submit-button" type="submit">
+        SEARCH
+      </button>
+    </form>
   );
 }
 
