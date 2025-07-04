494f9644357183e1cfaa0f9fd79d359fab194ae6
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-07-04 12:57:19 -0500

    make website suitable for mobile

--- hello-vite/src/components/Profile/Profile.jsx
+++ hello-vite/src/components/Profile/Profile.jsx
@@ -1,7 +1,6 @@
 import "./Profile.css";
 import { Link } from "react-router-dom";
 import chicken from "../../assets/chicken.jpg";
-import github from "../../assets/github.svg";
 
 function Profile({}) {
   return (
@@ -9,18 +8,20 @@ function Profile({}) {
       <p className="profile__title">
         Meet the Website Developer: Lydia Lockhart
       </p>
-      <img
-        className="profile__picture"
-        src={chicken}
-        alt="photo of Lydia, website creator, holding a chicken"
-      />
       <div className="profile__description">
-        This is where I put the description. Something about me. This is my bio.
         <img
-          className="profile__description-github"
-          src={github}
-          alt="github icon"
+          className="profile__picture"
+          src={chicken}
+          alt="photo of Lydia, website creator, holding a chicken"
         />
+        <div className="profile__bio">
+          Hi! My name is Lydia Lockhart, and I am a social services professional
+          who has learned to code! My love of learning human languages led me to
+          be interested in computer languages. As I explored coding, I was
+          intrigued by how communicating with computers, I could design
+          interactive websites. I decided to apply my software development
+          skills to design an app youth could use to find resources near them.
+        </div>
       </div>
     </div>
   );
