379fdb8399ea47a833d02fb89b403c3ed295ffbd
LYDIA LOCKHART <LYDIA.LOCKHART4@GMAIL.COM>
2025-07-04 23:36:52 -0500

    attempt an improvement, but abandon due to time

--- hello-vite/src/components/App/App.jsx
+++ hello-vite/src/components/App/App.jsx
@@ -28,14 +28,6 @@ function App() {
     return setNothingFoundStatus(false);
   }
 
-  function handleImproperSubmit(inputValue) {
-    const number = inputValue.length;
-    if (number === 0) {
-      return setNothingFoundStatus(true);
-    }
-    return result;
-  }
-
   const onZipSubmit = (e) => {
     e.preventDefault();
     console.log("Searching for zip:", inputValue);
